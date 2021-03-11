3d17f0dbfa0494df4e4ec81e65e3c9b76d107196
Navya <navyanikhil10@gmail.com>
2021-03-11 22:19:00 +0530

    Updated login credentials

--- app/src/main/java/com/cit/american_college_app/Login_Activity.kt
+++ app/src/main/java/com/cit/american_college_app/Login_Activity.kt
@@ -5,21 +5,37 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import org.w3c.dom.Text
 
 class Login_Activity : AppCompatActivity() {
     lateinit var btn:Button
     lateinit var txt:TextView
     lateinit var txt1:TextView
+    lateinit var uid:TextView
+    lateinit var pwd:TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         btn=findViewById(R.id.btnLogin)
         txt=findViewById(R.id.txtRegisterYourself)
         txt1=findViewById(R.id.txtForgotPassword)
+        uid=findViewById(R.id.userid)
+        pwd=findViewById(R.id.Password)
+        val validid="app_123"
+        val passw="password"
         btn.setOnClickListener{
-            var intent= Intent(this@Login_Activity,MainActivity::class.java)
-            startActivity(intent)
+            val userid=uid.text.toString()
+            val pass=pwd.text.toString()
+            if((userid==validid) && (pass==passw)) {
+
+
+                var intent = Intent(this@Login_Activity, MainActivity::class.java)
+                startActivity(intent)
+            }
+            else{
+                Toast.makeText(this@Login_Activity,"Incorrect Credentials",Toast.LENGTH_LONG).show()
+            }
         }
         txt.setOnClickListener{
             var i=Intent(this@Login_Activity,Signup_Activity::class.java)
